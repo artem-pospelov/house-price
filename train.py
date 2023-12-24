db813abb728998aa9ae32bd83e6dde866dd6e547
Artem Pospelov <71633210+artem-pospelov@users.noreply.github.com>
2023-12-24 20:46:16 +0300

    Update train.py

--- train.py
+++ train.py
@@ -2,14 +2,11 @@ import os
 
 import hydra
 import joblib
+import numpy as np
 import pandas as pd
 from omegaconf import DictConfig
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import (
-    mean_absolute_error,
-    mean_absolute_percentage_error,
-    mean_squared_error,
-)
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 from torch.utils.tensorboard import SummaryWriter
 
 
@@ -34,17 +31,17 @@ def train_model(cfg: DictConfig):
 
     mse = mean_squared_error(y_train, y_pred)
     mae = mean_absolute_error(y_train, y_pred)
-    mape = mean_absolute_percentage_error(y_train, y_pred)
+    rmse = np.sqrt(mse)
 
     y_train = pd.read_csv("y_train.csv")
 
-    metrics_names = ["mse", "mae", "mape"]
-    meitrics = [mse, mae, mape]
+    metrics_names = ["mse", "mae", "rmse"]
+    meitrics = [mse, mae, rmse]
 
     for metric, name in zip(meitrics, metrics_names):
         sw.add_scalar(name, metric, global_step=0)
 
-    return {"MSE": mse, "MAE": mae, "MAPE": mape}
+    return {"MSE": mse, "MAE": mae, "RMSE": rmse}
 
 
 if __name__ == "__main__":
