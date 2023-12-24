937588102de1489b260beb39e4b4b06ed457b443
artempospelov <pospelovartem@gmail.com>
2023-12-24 18:26:03 +0300

    Initial commit

---
+++ infer.py
@@ -0,0 +1,30 @@
+import os
+
+import hydra
+import joblib
+import pandas as pd
+
+
+@hydra.main(version_base=None, config_path="configs", config_name="configs")
+def run_inference(cfg):
+    # Load model
+    model = joblib.load(cfg.model_path)
+
+    # Load input data
+    X_test = pd.read_csv(cfg.X_test_path)
+
+    # Make predictions
+    y_pred = model.predict(X_test)
+    y_pred_df = pd.DataFrame(y_pred)
+    y_pred_df.to_csv(cfg.predicted_path, index=False)
+    # return predictions
+    return y_pred
+
+
+if __name__ == "__main__":
+    os.system("dvc fetch model.joblib")
+    os.system("dvc fetch X_test.csv")
+    os.system("dvc pull --remote myremote")
+    run_inference()
+    os.system("dvc add predicted.csv")
+    os.system("dvc push predicted.csv.dvc")
---
+++ train.py
@@ -0,0 +1,61 @@
+import os
+
+import hydra
+import joblib
+import pandas as pd
+from omegaconf import DictConfig
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import (
+    mean_absolute_error,
+    mean_absolute_percentage_error,
+    mean_squared_error,
+)
+from torch.utils.tensorboard import SummaryWriter
+
+
+@hydra.main(version_base=None, config_path="configs", config_name="config")
+def train_model(cfg: DictConfig):
+    # Load data
+    X_train = pd.read_csv(cfg.X_train_path)
+    y_train = pd.read_csv(cfg.X_train_path)
+
+    # Train model
+    model = RandomForestRegressor(
+        n_estimators=cfg.n_estimators,
+        max_depth=cfg.max_depth,
+        min_samples_split=cfg.min_samples_split,
+        min_samples_leaf=cfg.min_samples_leaf,
+    )
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_train)
+
+    # Save model
+    joblib.dump(model, cfg.model_path)
+
+    return y_pred
+
+
+def metrics(y_train, y_pred):
+    mse = mean_squared_error(y_train, y_pred)
+    mae = mean_absolute_error(y_train, y_pred)
+    mape = mean_absolute_percentage_error(y_train, y_pred)
+
+    metrics_names = ["mse", "mae", "mape"]
+    meitrics = [mse, mae, mape]
+
+    for metric, name in zip(meitrics, metrics_names):
+        sw.add_scalar(name, metric, global_step=0)
+
+    return {"MSE": mse, "MAE": mae, "MAPE": mape}
+
+
+if __name__ == "__main__":
+    sw = SummaryWriter("exp_logs")
+    os.system("dvc fetch X_train.csv")
+    os.system("dvc fetch y_train.csv")
+    os.system("dvc pull --remote myremote")
+    y_pred = train_model()
+    y_train = pd.read_csv("y_train.csv")
+    metrics(y_train, y_pred)
+    os.system("dvc add model.joblib")
+    os.system("dvc push model.joblib.dvc")
